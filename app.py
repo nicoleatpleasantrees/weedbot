0255b260722fd244bbbe0120dfc627d58d857a46
Nicole Lillian Mark <150384827+nicoleatpleasantrees@users.noreply.github.com>
2023-11-28 21:51:31 +0000

    Add requirements,txt, update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+from streamlit_gsheets import GSheetsConnection
+
+conn = st.connection("gsheets",type=GSheetsConnection)
+df = conn.read(worksheet= "mra_data", nrows=25)
+
 st.title('Pleasantrees Product Explorer')
 
-st.connections.SnowflakeConnection('pmb32940.us-east-1.snowflakecomputing.com')
+# st.connections.SnowflakeConnection('pmb32940.us-east-1.snowflakecomputing.com')
 
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
