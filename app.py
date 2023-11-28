6e0c22bb71052cd9b4fe9300bc8c705df293b408
Nicole Lillian Mark <150384827+nicoleatpleasantrees@users.noreply.github.com>
2023-11-28 22:35:52 +0000

    Remove Snowflake connection

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import pandas as pd
-
 from streamlit_gsheets import GSheetsConnection
 
 conn = st.connection("gsheets",type=GSheetsConnection)
-df = conn.read(worksheet= "mra_data", nrows=25)
+df = conn.read(worksheet= "318772191", nrows=25)
 
 st.title('Pleasantrees Product Explorer')
-
-# st.connections.SnowflakeConnection('pmb32940.us-east-1.snowflakecomputing.com')
-
 x = st.slider('Select a value')
 st.write(x, 'squared is', x * x)
 
