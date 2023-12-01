56e4e794d1989678374fcbd2758d0e71a176d099
Nicole Lillian Mark <150384827+nicoleatpleasantrees@users.noreply.github.com>
2023-12-01 21:15:10 +0000

    Add metrics and columns

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 import streamlit as st
 import pandas as pd
-from streamlit_gsheets import GSheetsConnection
+from st_files_connection import FilesConnection
 
-conn = st.experimental_connection("gsheets",type=GSheetsConnection)
-df = conn.read(spreadsheet = "", nrows=25)
+conn = st.connection('s3', type=FilesConnection)
+df = conn.read('data-streamlit-nicole/recommender_text_v3 - rec_text.csv', input_format='csv', ttl=600)
 
 st.title('Pleasantrees Product Explorer')
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+# x = st.slider('Select a value')
+# st.write(x, 'squared is', x * x)
+
+st.dataframe(df)
+n = len(pd.unique(df['product_id']))
+
+col1, col2, col3 = st.columns(spec=3, gap="large")
+
+with col1:  
+    st.header("PRODUCTS")
+
+with col2:
+    st.header("BRANDS")
+
+with col3:
+    st.header("CATEGORIES")
+
+
+st.metric('Number of Products', n)
+m = len(pd.unique(df['brand_name']))
+st.metric('Number of Brands', m)
+cat = df.category
 
 add_selectbox = st.sidebar.selectbox(
-    'How would you like to find the perfect product today?',
+    'How would you like to find products today?',
     ('Browse by category', 'Browse by brand', 'Ask AI!')
 )
+
+if add_selectbox == 'Browse by category':
+    st.write(cat)
+    
+    
