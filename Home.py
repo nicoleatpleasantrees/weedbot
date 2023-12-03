f27e264e6886e78466cf83052297e179bddbaf15
Nicole Lillian Mark <150384827+nicoleatpleasantrees@users.noreply.github.com>
2023-12-03 19:31:30 +0000

    Created pages/Section A.py, pages/Section B.py

--- Home.py
+++ Home.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import replicate
 from st_files_connection import FilesConnection
 
 st.set_page_config(
@@ -16,26 +15,3 @@ with st.sidebar:
     st.header('Product Explorer')
     st.subheader('Explore our products and find the right one for you!')
     st.caption('Made with ❤️ by Nicole')
-   
-    if 'REPLICATE_API_TOKEN' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    else:
-        replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-    
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B', 'Llama2-70B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    else:
-        llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
-
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider(('max_length'), min_value=1, max_value=100, value=20, step=1)
---
+++ pages/About.py
@@ -0,0 +1,2 @@
+st.title('About Pleasantrees')
+st.image('https://i.ibb.co/FJR389Y/Pleasantrees-logo-sxs-white.png', width=400)
