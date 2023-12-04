cfe595b7444fef51f17792566b1ef5b3d7374534
Nicole Lillian Mark <150384827+nicoleatpleasantrees@users.noreply.github.com>
2023-12-04 21:27:39 +0000

    Finish chatbot

--- pages/Weedbot.py
+++ pages/Weedbot.py
@@ -4,7 +4,7 @@ from hugchat import hugchat
 from hugchat.login import Login
 
 st.image('https://i.postimg.cc/GpxSW0bG/Pleasantrees-logo-sxs-white.png', width=400)
-st.title('Weedbot')
+st.subheader('Weedbot')
 
 with st.sidebar:
     st.subheader("Enter your Hugging Face credentials")
@@ -20,8 +20,30 @@ with st.sidebar:
           else:
               st.success('Login successful! You may now proceed to the weed...')       
 if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+    st.session_state.messages = [{"role": "assistant", "content": "What questions fo you have for the Weedbot🔮?"}]
     for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+def generate_response(prompt_input, email, passwd):
+    # Hugging Face Login
+    sign = Login(email, passwd)
+    cookies = sign.login()
+    # Create ChatBot                        
+    chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+    return chatbot.chat(prompt_input)
+
+if prompt := st.chat_input(disabled=not (hf_email and hf_password)):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_response(prompt, hf_email, hf_password) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)        
+
 
