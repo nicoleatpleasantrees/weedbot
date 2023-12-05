f22f05f07a95466a0ddea354c6c5e686ebabce42
Nicole Lillian Mark <150384827+nicoleatpleasantrees@users.noreply.github.com>
2023-12-05 17:41:55 +0000

    Add treemap to Charts.py

--- pages/Charts.py
+++ pages/Charts.py
@@ -3,12 +3,13 @@ import plotly.express as px
 from st_files_connection import FilesConnection
 
 st.image('https://i.ibb.co/FJR389Y/Pleasantrees-logo-sxs-white.png', width=400)
-st.title('Charts')
 
 conn = st.connection('s3', type=FilesConnection)
 df = conn.read('data-streamlit-nicole/strains_for_plotly.csv')
 
+
 st.subheader("Strains by potency")
+st.markdown("This chart shows the distribution of strains by potency range.")
 
 fig = px.histogram(
     data_frame=df, x=df["average THC"],
@@ -17,3 +18,15 @@ fig = px.histogram(
     hover_data=df.columns
     )
 st.plotly_chart(fig, use_container_width=False,sharing='streamlit', theme='streamlit')
+
+st.subheader("Strains by type")
+
+fig_2 = px.treemap(
+    data_frame=df, path=[px.Constant("all"), "type", "strain"],
+    values="average THC",
+    color="average THC",
+    color_continuous_scale="RdBu",
+    hover_data=["average THC"],
+    )
+
+st.plotly_chart(fig_2, use_container_width=True,sharing='streamlit', theme='streamlit')
