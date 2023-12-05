c06f39caa14e69d14f831bce93ebf850450eed74
Nicole Lillian Mark <150384827+nicoleatpleasantrees@users.noreply.github.com>
2023-12-05 17:31:46 +0000

    Add histogram to Charts.py

--- pages/Charts.py
+++ pages/Charts.py
@@ -9,4 +9,11 @@ conn = st.connection('s3', type=FilesConnection)
 df = conn.read('data-streamlit-nicole/strains_for_plotly.csv')
 
 st.subheader("Strains by potency")
-st.write("This chart shows the distribution of strains by potency."
+
+fig = px.histogram(
+    data_frame=df, x=df["average THC"],
+    color=df["type"], marginal="rug",
+    nbins=8,
+    hover_data=df.columns
+    )
+st.plotly_chart(fig, use_container_width=False,sharing='streamlit', theme='streamlit')
